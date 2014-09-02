12185553e69776b03b39842c11a1895e27234d01
Jeff Gayle <jeff.j.gayle@gmail.com>
2014-09-02 10:00:00 -0700

    Added log statement

--- GroupGithubTesting/ViewController.swift
+++ GroupGithubTesting/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        println("Merge Conflict")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
